11863c08813302bc7259fe5194f73580214ec3b7
devanaclimgo <anaclimgo@gmail.com>
2024-09-08 18:38:21 -0300

    adicionei as tags

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ let filmes = [
     descricao:
       "Em 'Harry Potter e a Câmara Secreta', o segundo livro da saga escrita por J.K. Rowling, Harry Potter retorna para Hogwarts, a Escola de Magia e Bruxaria, para mais um ano repleto de aventuras e mistérios. No entanto, algo sinistro paira sobre o castelo: a lenda da Câmara Secreta, um lugar onde um monstro aterroriza os alunos trouxas (não-magos).",
     link: "https://harrypotter.fandom.com/pt-br/wiki/C%C3%A2mara_Secreta",
+    tags: "basilisco cobra tom riddle canos"
   },
   {
     titulo: "Harry Potter e o Cálice de Fogo",
     descricao:
       "Em 'Harry Potter e o Cálice de Fogo', Harry Potter é misteriosamente inscrito no Torneio Tribruxo, uma competição mágica entre três das maiores escolas de magia da Europa. Acompanhe Harry em uma jornada repleta de desafios e perigos enquanto ele luta para sobreviver.",
     link: "https://harrypotter.fandom.com/pt-br/wiki/C%C3%A1lice_de_Fogo",
+    tags: "labirinto desafios provas baile de inverno"
   },
   {
     titulo: "Harry Potter e o Enigma do Príncipe",
     descricao:
       "Em 'Harry Potter e o Enigma do Príncipe', Lord Voldemort intensifica sua busca pelo poder, e Harry e seus amigos devem se preparar para a batalha final. Descubra os segredos do passado de Voldemort e ajude Harry a desvendar o enigma do Príncipe Mestiço.",
     link: "https://harrypotter.fandom.com/pt-br/wiki/Enigma_do_Pr%C3%ADncipe",
+    tags: "snape dumbledore regulus black horcrux horcruxes"
   },
 ];
 
@@ -38,13 +41,15 @@ function pesquisar() {
   let resultados = "";
   let titulo = "";
   let descricao = "";
+  let tags = "";
 
   // Itera sobre cada dado da lista de dados
   for (let dado of filmes) {
     titulo = dado.titulo.toLowerCase();
     descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
     // se titulo includes campoPesquisa
-    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
       // cria um novo elemento
       resultados += `
       <div class="item-resultados">
@@ -58,7 +63,7 @@ function pesquisar() {
   if (!resultados) {
     resultados = "<p>Nada foi encontrado</p>";
   }
-  
+
   // Atribui os resultados gerados à seção HTML
   section.innerHTML = resultados;
 }
