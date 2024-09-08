26df5bf08b360e326e2f2db946b8a36c5d7dabc6
devanaclimgo <anaclimgo@gmail.com>
2024-09-08 18:23:38 -0300

    javascript funcionando, adicionei uma função e fiz a barra de pesquisa funcionar

--- script.js
+++ script.js
@@ -19,21 +19,47 @@ let filmes = [
   },
 ];
 
-
 function pesquisar() {
+  // Obtém a seção HTML onde os resultados serão exibidos
   let section = document.getElementById("resultados-pesquisa");
-  
-  let resultados = ""
-  
-  for(let dado of filmes) {
-    resultados += `
-    <div class="item-resultados">
-      <h2>${dado.titulo}</h2>
-      <p>${dado.descricao}</p>
-      <a href=${dado.link} target="_blank">Mais informações ↗</a>
-    </div>
-    `;
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML =
+      "<p>Nada foi encontrado.</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+
+  // Itera sobre cada dado da lista de dados
+  for (let dado of filmes) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+      // cria um novo elemento
+      resultados += `
+      <div class="item-resultados">
+        <h2>${dado.titulo}</h2>
+        <p>${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações ↗</a>
+      </div>
+      `;
+    }
+  }
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>";
   }
   
+  // Atribui os resultados gerados à seção HTML
   section.innerHTML = resultados;
 }
+
